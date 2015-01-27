574ee5e69ce526cbabefd55761349b12a853a85e
Mike Ackerman <mikeackerman@gmail.com>
2015-01-26 18:50:04 -0700

    knockout.js map and location list wired up

--- js/app.js
+++ js/app.js
@@ -4,181 +4,219 @@
 
 var allLocations = [
     {
-      title: "Viget (My Work)",
-      lat: 40.0173,
-      lng: -105.277
+        id: 1,
+        title: "Avery Brewing Company",
+        address: "5763 Arapahoe Avenue, Boulder, CO 80303",
+        lat: 40.0144,
+        lng: -105.219
     },
     {
-      title: "PMG (Wife's Work)",
-      lat: 40.0175,
-      lng: -105.282
+        id: 2,
+        title: "Twisted Pine Brewing Co",
+        address: "3201 Walnut Street, Boulder, CO 80301",
+        lat: 40.0201,
+        lng: -105.251
     },
     {
-      title: "Boulder Fermentation Supply",
-      lat: 40.0264,
-      lng: -105.244
+        id: 3,
+        title: "Boulder Beer",
+        address: "2880 Wilderness Place, Boulder, CO 80301",
+        lat: 40.0261,
+        lng: -105.248
     },
     {
-      title: "Outback Saloon",
-      lat: 40.0317,
-      lng: -105.258
+        id: 4,
+        title: "Upslope Brewing Company",
+        address: "1898 South Flatiron Court Boulder, CO 80301",
+        lat: 40.0203,
+        lng: -105.218
     },
     {
-      title: "Backcountry Pizza & Taphouse",
-      lat: 40.0144,
-      lng: -105.264
+        id: 5,
+        title: "FATE Brewing Company",
+        address: "1600 38th Street, Boulder, CO 80301",
+        lat: 40.0145,
+        lng: -105.245
+    },
+    {
+        id: 6,
+        title: "J Wells Brewery",
+        address: "2516 49th Street #5, Boulder, CO 80301",
+        lat: 40.0245,
+        lng: -105.239
+    },
+    {
+        id: 7,
+        title: "Wild Woods Brewery",
+        address: "5460 Conestoga Court, Boulder, CO 80301",
+        lat: 40.0166,
+        lng: -105.227
+    },
+    {
+        id: 8,
+        title: "Mountain Sun Pub & Brewery",
+        address: "1535 Pearl Street, Boulder, CO 80302",
+        lat: 40.0188,
+        lng: -105.275
+    },
+    {
+        id: 9,
+        title: "Walnut Brewery",
+        address: "1123 Walnut Street, Boulder, CO 80302",
+        lat: 40.0167,
+        lng: -105.281
+    },
+    {
+        id: 10,
+        title: "Sanitas Brewing Co",
+        address: "3550 Frontier Avenue, Boulder, CO 80301",
+        lat: 40.0231,
+        lng: -105.247
+    },
+    {
+        id: 11,
+        title: "BRU handbuilt ales & eats",
+        address: "5290 Arapahoe Avenue, Boulder, CO 80303",
+        lat: 40.0145,
+        lng: -105.228
+    },
+    {
+        id: 12,
+        title: "Boulder Distillery & Clear Spirit Company",
+        address: "2500 47th Street Unit 10, Boulder, CO 80301",
+        lat: 40.0264,
+        lng: -105.244
+    },
+    {
+        id: 13,
+        title: "Roundhouse Spirits Distillery",
+        address: "5311 Western Avenue #180, Boulder, CO 80301",
+        lat: 40.0174,
+        lng: -105.229
+    },
+    {
+        id: 14,
+        title: "J&L Distilling Company",
+        address: "4843 Pearl Street #1, Boulder, CO 80301",
+        lat: 40.0254,
+        lng: -105.240
     }
 ]
 
 /* ======= Model ======= */
 
-var Location = function() {
+var Location = function(data, owner) {
+    var self = this;
 
+    this.id = ko.observable("location-" + data.id);
+    this.title = ko.observable(data.title);
+    this.address = ko.observable(data.address);
+    this.latitude = ko.observable(data.lat);
+    this.longitude = ko.observable(data.lng);
+
+    this.createMarker = function() {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(ko.unwrap(this.latitude), ko.unwrap(this.longitude)),
+            title: ko.unwrap(this.title),
+            map: owner.map
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            owner.locationSelected(self);
+        });
+
+        return marker;
+    }
+
+    var marker = this.createMarker();
 }
 
 /* ======= ViewModel ======= */
 
-// 1. update my app to use knockout
-// 1.1 create ViewModel functions for listview contents and clicks
-// 1.2 listview click setCurrentLocation() on ViewModel which updates observables in info section
 // 1.3 make viewModel update map markers? -- map marker clicks? -- map marker infowindows closing on selection
 // 1.4 make listview click update css property to show selected
 
 var ViewModel = function() {
+    var self = this;
 
-    // 1. he starts with putting the model inside the ViewModel (name: clickCount: imgSrc:, imgAttribution, incrementCounter function())
-    // 2. he puts data-bind attributes in the DOM and removes the ids for the current cat
-    // 2.1 for img src he does data-bind="attr: {src: imgSrc}"
-    // 3. add an obersvable array in viewModel -. added a ul with foreach: nicknames: and li with data-bind="text: $data"
-    // 4. moves all the "model" related code from ViewModel to a Cat function (all the properties and computed functdions)
-    // 5. in ViewModel this.currentCat = ko.observable(new Cat() );
-    // 6. in DOM changes all data-bind things to currentCat().name
-    // 7. changes currentCount function from this. to this.currentCat().
-    // 8. uses KO `with` for scoping to remove "currentCat()" all over the place in DOM (div data-bind="with:currentCat") ALSO changes click to $parent.incrementCounter
-    // 9. adds the var self = this; for the incrementCounter function scope within currentCat thing
-    // 10. updates Cat function to accept data arg (var Cat = function(data) )
-    // 11. change cat this.name = ko.observable(data.name) and hard-codes the { name: 'hi' } data in the ViewModel at constructor
-    // 12. added var initialCats = [] array like previous
-    // 13. in ViewModel, this.catLast = ko.observableArray([]); then initialCats.forEach(function(catItem) {
-        // self.catList.push(new Cat(catItem));
-    // })
-    // 14. this.currentCat = ko.obserable(this.catList()[0]);
-    // 15. to make them show up iterate over list in DOM and make function to set currentCat observable on click
+    this.locations = ko.observableArray([]);
+    this.currentLocation = ko.observable(null);
 
-}
-
-/* ======= View ======= */
-
-var mapView = {
-
-    markers: [],
-    infoWindows: [],
-
-    init: function(aMap) {
-        var mapOptions = {
-          center: { lat: 40.025, lng: -105.27},
-          zoom: 14
-        };
-
-        // store the map for later and attach it to the DOM
-        this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-        this.render();
-    },
-
-    render: function() {
-        // populate markers
-        var locations = ViewModel.getLocations();
-
-        for (var i = 0; i < locations.length; i++) {
-            this.markers[i] = new google.maps.Marker({
-                position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-                map: this.map,
-                title: locations[i].title
-            });
-
-            this.infoWindows[i] = new google.maps.InfoWindow({
-                content: "<div>" + locations[i].title + "</div>"
-            });
-
-            google.maps.event.addListener(this.markers[i], 'click', this.markerClickCallback(i, this));
-        }
-    },
-
-    markerClickCallback: function(index, that) {
-        return function() {
-            that.infoWindows[index].open(that.map, that.markers[index]);
-        };
-    },
-
-    closeOpenInfoWindows: function() {
-        for (var i = 0; i < this.infoWindows.length; i++) {
-            this.infoWindows[i].close();
-        }
-    },
-
-    openInfoWindow: function(index) {
-        this.closeOpenInfoWindows();
-        this.infoWindows[index].open(this.map, this.markers[index]);
+    this.locationSelected = function(location) {
+        this.currentLocation(location);
+        document.getElementById(ko.unwrap(location.id)).scrollIntoView();
     }
-}
 
-var locationListView = {
-    init: function() {
-        // store the DOM element for access later
-        this.locationListElem = document.getElementById('location-list');
+    this.initialize = function() {
+        this.map = new google.maps.Map(document.getElementById('map-canvas'), {
+            center: new google.maps.LatLng(40.025, -105.27),
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        })
 
-        this.render();
-    },
-
-    render: function() {
-        var location, elem, i;
-
-        // get the locations from the ViewModel
-        var locations = ViewModel.getLocations();
-
-        // empty the location list
-        this.locationListElem.innerHTML = '';
-
-        // loop over the locations
-        for (i = 0; i < locations.length; i++) {
-            location = locations[i];
-
-            elem = document.createElement('li');
-            elem.textContent = location.title;
-
-            // on click. setCurrentLocation and render the locationView
-            elem.addEventListener('click', (function(locationCopy) {
-                return function() {
-                    ViewModel.setCurrentLocation(locationCopy);
-                    locationView.render();
-                    mapView.openInfoWindow(i);
-                };
-            })(location));
-
-            // finally, add the element to the list
-            this.locationListElem.appendChild(elem);
-        }
+        allLocations.forEach(function(locationItem) {
+            self.locations.push(new Location(locationItem, this));
+        });
     }
+
+    initialize();
 }
 
-var locationView = {
-    init: function() {
-        // store pointers to DOM elements for access later
-        this.locationNameElem = document.getElementById('location-name');
 
-        this.render();
-    },
+// var mapView = {
 
-    render: function() {
-        // update the DOM elements with values from the current location
-        var currentLocation = ViewModel.getCurrentLocation();
+//     markers: [],
+//     infoWindows: [],
 
-        if (currentLocation !== null) {
-            this.locationNameElem.textContent = currentLocation.title;
-        }
-    }
-}
+//     init: function(aMap) {
+//         var mapOptions = {
+//           center: { lat: 40.025, lng: -105.27},
+//           zoom: 14
+//         };
+
+//         // store the map for later and attach it to the DOM
+//         this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+//         this.render();
+//     },
+
+//     render: function() {
+//         // populate markers
+//         var locations = ViewModel.getLocations();
+
+//         for (var i = 0; i < locations.length; i++) {
+//             this.markers[i] = new google.maps.Marker({
+//                 position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+//                 map: this.map,
+//                 title: locations[i].title
+//             });
+
+//             this.infoWindows[i] = new google.maps.InfoWindow({
+//                 content: "<div>" + locations[i].title + "</div>"
+//             });
+
+//             google.maps.event.addListener(this.markers[i], 'click', this.markerClickCallback(i, this));
+//         }
+//     },
+
+//     markerClickCallback: function(index, that) {
+//         return function() {
+//             that.infoWindows[index].open(that.map, that.markers[index]);
+//         };
+//     },
+
+//     closeOpenInfoWindows: function() {
+//         for (var i = 0; i < this.infoWindows.length; i++) {
+//             this.infoWindows[i].close();
+//         }
+//     },
+
+//     openInfoWindow: function(index) {
+//         this.closeOpenInfoWindows();
+//         this.infoWindows[index].open(this.map, this.markers[index]);
+//     }
+// }
+
 
 // make it go!
 ko.applyBindings(ViewModel);
