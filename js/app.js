4f0980594f1af6a9e3f4e16e9bf274b4723bc088
Mike Ackerman <mikeackerman@gmail.com>
2015-02-12 17:08:47 -0700

    [add] JSDoc comments :-/, make it responsive-ish.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+/**
+ * app.js
+ * This is the file that houses the main app logic. It includes initial location data,
+ * two Models (Location and Beer), a ViewModel, and initializes knockout.
+ */
 
-// 2. find and add images for locations
-// 3. yelp reviews for locations
-
+ /**
+  * Initial Location data
+  * It includes: id, title, address, latitude, longitude, BreweryDB API Id, image
+  */
 var allLocations = [
     {
         id: 1,
@@ -104,73 +110,100 @@ var allLocations = [
     }
 ]
 
-/* ======= Model ======= */
-
+/**
+ * The Location Model.
+ * It sets the publicly viewable knockout attributes, handles creating a map marker,
+ * handles creating a map info window (and opening/closing), and fetching beer data from the API.
+ */
 var Location = function(data, owner) {
     var self = this;
 
-    this.id = ko.observable("location-" + data.id);
-    this.title = ko.observable(data.title);
-    this.address = ko.observable(data.address);
-    this.latitude = ko.observable(data.lat);
-    this.longitude = ko.observable(data.lng);
-    this.image = ko.observable(data.image);
+    // knockout observables
+    this.id         = ko.observable("location-" + data.id);
+    this.title      = ko.observable(data.title);
+    this.address    = ko.observable(data.address);
+    this.latitude   = ko.observable(data.lat);
+    this.longitude  = ko.observable(data.lng);
+    this.image      = ko.observable(data.image);
     this.dataLoaded = ko.observable(false);
-    this.breweryDbId = data.breweryDbId;
-    this.owner = owner;
+    this.beers      = ko.observableArray([]);
 
+    // instance variables
+    this.owner         = owner;
+    this.contentString = "<p>" + this.title() + "</p>";
+    this.breweryDbId   = data.breweryDbId;
+
+    /**
+     * This function creates a google map marker object
+     * @return {object} This returns the marker at lat/lng, with title and click handler.
+     */
     this.createMarker = function() {
+        // create the Google Maps marker at the correct location with title
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(this.latitude(), this.longitude()),
             title: this.title(),
             map: owner.map
         });
 
+        // set the click handler -- which sets ViewModel.locationSelected(self)
         google.maps.event.addListener(marker, 'click', function() {
             owner.locationSelected(self);
         });
 
         return marker;
     }
-
     this.marker = this.createMarker();
 
-    this.contentString = "<p>" + this.title() + "</p>";
-
+    /**
+     * This function creates a google map info window object
+     * @return {object} This returns the info window with content.
+     */
     this.createInfoWindow = function() {
+        // create the info window and set the content.
+        // had to initialize the string outside of this function ¯\_(ツ)_/¯
         var infoWindow = new google.maps.InfoWindow({
             content: self.contentString
         });
 
         return infoWindow;
     }
+    this.infoWindow = this.createInfoWindow();
 
+    /**
+     * This function opens the location's info window
+     */
     this.openInfoWindow = function() {
         self.infoWindow.open(owner.map, self.marker);
     }
 
+    /**
+     * This function closes the location's info window
+     */
     this.closeInfoWindow = function() {
         self.infoWindow.close();
     }
 
-    this.infoWindow = this.createInfoWindow();
-
-    this.beers = ko.observableArray([]);
-
+    /**
+     * This function loads beer data from the Brewery DB API (proxy).
+     * On success: set the location's beers collection & flag the location as having loaded data.
+     * On failure: alert the user with the error message.
+     */
     this.loadBeerData = function() {
         $.ajax({ url: "http://mackermedia.com/brewery_db_proxy/?brewery_id=" + self.breweryDbId,
             success: function(data) {
                 self.setBeers(data.data);
                 self.dataLoaded(true);
-                self.owner.triggerCarousel();
             },
             error: function(msg) {
-                // TODO show an error message
-                console.log(msg);
-                alert('oh no');
+                alert(msg);
             }});
     }
 
+    /**
+     * This function sets the location's beers collection
+     * It ignores beers with availability of 'Not Available'.
+     * @param  {array of objects} This is the beer data to be stored in the beers collection.
+     */
     this.setBeers = function(data) {
         data.forEach(function (beerData) {
             if (beerData.availabilityId !== undefined && beerData.availabilityId === 3) {
@@ -182,14 +215,24 @@ var Location = function(data, owner) {
     }
 }
 
+/**
+ * The Beer Model.
+ * It sets the publicly viewable knockout attributes.
+ * @param {object} This is the beer data to be set on the observable attributes.
+ */
 var Beer = function(data) {
+    // knockout observables
     this.name = ko.observable(data.name);
     this.abv = ko.observable(data.abv);
+
+    // handle case where no picture
     if (data.labels !== undefined) {
         this.icon = ko.observable(data.labels.medium);
     } else {
         this.icon = ko.observable("images/na.png");
     }
+
+    // handle case where no style information
     if (data.style !== undefined) {
         this.style = ko.observable(data.style.name);
     } else {
@@ -197,29 +240,49 @@ var Beer = function(data) {
     }
 }
 
-/* ======= ViewModel ======= */
-
-
+/**
+ * The ViewModel.
+ * A knockout.js convention object for handling binding data between the Models and the View.
+ * It handles providing observable data and special application logic.
+ */
 var ViewModel = function() {
     var self = this;
 
+    // knockout observables initialization
     this.locations = ko.observableArray([]);
     this.filteredLocations = ko.observableArray([]);
     this.currentLocation = ko.observable(null);
     this.searchKeyword = ko.observable('');
 
+    /**
+     * This function updates the currently selected Location.
+     * On setting current: scroll the Location ListView item into view,
+     * close all open info windows, open the location's info window, & load beer data.
+     * @param  {object} This is the location object to set as current.
+     */
     this.locationSelected = function(location) {
+        // set the ko observable
         self.currentLocation(location);
+        // scroll the listview item into view
         $("#" + location.id()).scrollintoview();
+
+        // close open info windows and open the only current one
         closeAllInfoWindows();
         location.openInfoWindow();
+
+        // load beer data
         if (location.dataLoaded() !== true) {
             location.loadBeerData();
         }
     }
 
+    /**
+     * This function gets called on creating a new ViewModel object.
+     * It initializes the Google Maps map, initializes Location data, and filtered location data.
+     */
     this.initialize = function() {
-        this.map = new google.maps.Map(document.getElementById('map-canvas'), {
+        // init the google map
+        self.map = new google.maps.Map(document.getElementById('map-canvas'), {
             center: new google.maps.LatLng(40.025, -105.27),
             draggable: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -228,21 +291,32 @@ var ViewModel = function() {
             zoom: 13,
         })
 
+        // listen for the window resize event & trigger Google Maps to update too
+        $(window).resize(function() {
+            var center = map.getCenter();
+            google.maps.event.trigger(this.map, "resize");
+            self.map.setCenter(center);
+        });
+
         // init all locations list
         allLocations.forEach(function(locationItem) {
             self.locations.push(new Location(locationItem, this));
         });
 
-        // init filtered locations list
+        // init filtered locations list -- used for search/filtering
         self.locations().forEach(function (location) {
             self.filteredLocations.push(location);
         })
     }
 
+    /**
+     * This function is called on change of the search/filter input.
+     * It resets the map, determines matching locations, and then updates the map.
+     */
     this.searchFilter = function() {
         self.resetMap();
 
-        // determine matches
+        // determine matches by simple string contains matching
         var matches = self.locations().filter(function (location) {
             return location.title().toLowerCase().indexOf(self.searchKeyword()) !== -1;
         });
@@ -253,12 +327,18 @@ var ViewModel = function() {
         self.updateMap();
     }
 
+    /**
+     * This function resets the google map's markers by removing all of them.
+     */
     this.resetMap = function() {
         self.locations().forEach(function (location) {
             location.marker.setMap(null);
         })
     }
 
+    /**
+     * This function updates the google map's markers by adding only ones belonging to filteredLocations.
+     */
     this.updateMap = function() {
         self.filteredLocations().forEach(function (location) {
             location.marker.setMap(self.map);
@@ -266,6 +346,9 @@ var ViewModel = function() {
         })
     }
 
+    /**
+     * This function closes all of the google map's info windows.
+     */
     this.closeAllInfoWindows = function() {
         self.filteredLocations().forEach(function (location) {
             location.closeInfoWindow();
